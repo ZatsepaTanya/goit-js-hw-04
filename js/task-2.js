0d1b8fb3c69cabd49a8b3ba2f8de8cd89ef75204
ZatsepaTanya <Tanyamath@i.ua>
2020-12-24 00:58:36 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,13 @@
 const inventory = {
     items: ['Knife', 'Gas mask'],
+    
     add(itemName) {
       console.log(`Adding ${itemName} to inventory`);
   
       this.items.push(itemName);
+      
     },
+    
     remove(itemName) {
       console.log(`Removing ${itemName} from inventory`);
   
@@ -15,15 +18,14 @@ const inventory = {
   const invokeInventoryAction = function(itemName, action) {
     console.log(`Invoking action on ${itemName}`);
     action(itemName);
-  };
-  
-  invokeInventoryAction('Medkit', inventory.add);
+     };
+   invokeInventoryAction('Medkit', inventory.add.bind(inventory));
   // Invoking action on Medkit
   // Adding Medkit to inventory
   
   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
   
-  invokeInventoryAction('Gas mask', inventory.remove);
+  invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
   // Invoking action on Gas mask
   // Removing Gas mask from inventory
   
